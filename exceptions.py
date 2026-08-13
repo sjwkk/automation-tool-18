32ffdb108fa7dd962b0c508ba9fe0f85ca7c6229
Peter Horváth <fhcgcnc32@gmail.com>
2026-08-14 00:15:36 +0200

    custom exceptions for game errors

--- exceptions.py
+++ exceptions.py
@@ -1,23 +1,30 @@
 class GameError(Exception):
     def __init__(self, message):
         super().__init__(message)
+        self.message = message
 
 class PlayerNotFoundError(GameError):
     def __init__(self, player_id):
         super().__init__(f'Player with ID {player_id} not found.')
+        self.player_id = player_id
 
-class LevelLoadError(GameError):
-    def __init__(self, level_id):
-        super().__init__(f'Failed to load level with ID {level_id}.')
+class InvalidMoveError(GameError):
+    def __init__(self, message):
+        super().__init__(message)
 
-class ActionNotAllowedError(GameError):
-    def __init__(self, action):
-        super().__init__(f'Action {action} is not allowed.')
+class GameNotStartedError(GameError):
+    def __init__(self):
+        super().__init__('Game has not been started yet.')
 
-class ValidationError(GameError):
-    def __init__(self, errors):
-        super().__init__('Validation errors occurred.')
-        self.errors = errors
+class ConnectionError(GameError):
+    def __init__(self, message):
+        super().__init__(message)
 
-    def __str__(self):
-        return f'{super().__str__()}: {self.errors}'
+class InvalidInputError(GameError):
+    def __init__(self, input_value):
+        super().__init__(f'Invalid input: {input_value}')
+        self.input_value = input_value
+
+class GameFullError(GameError):
+    def __init__(self):
+        super().__init__('Game room is full.')
